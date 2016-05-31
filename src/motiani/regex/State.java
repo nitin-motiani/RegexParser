d3937e0acc0c5e837b93e8f2edba91aa063ef5e0
Nitin Motiani <nitin.motiani@gmail.com>
2016-05-31 17:33:27 +0530

    Crude testing of State class using main function

--- src/motiani/regex/State.java
+++ src/motiani/regex/State.java
@@ -1,5 +1,6 @@
 package motiani.regex;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -7,9 +8,23 @@ import java.util.Set;
 public class State {
 	private Map<Character, Set<State>> transitionMap;
 
+	State()
+	{
+		transitionMap = new HashMap<Character, Set<State>>();
+	}
+	
 	State(Map<Character, Set<State>> tMap)
 	{
 		transitionMap = tMap;
+	}
+	
+	void addMove(Character key, Set<State> value)
+	{
+		if(transitionMap.containsKey(key))
+			value.addAll(transitionMap.get(key));
+			
+		transitionMap.put(key, value);
+		
 	}
 	
 	private Set<State> emptyPathMoveHelper(Set<State> seenStates)
@@ -41,13 +56,15 @@ public class State {
 		return emptyPathMoveHelper(seenStates);
 	}
 	
+	/* 
+	 * Should we return null or empty set in case of no moves. 
+	 */
 	public Set<State> makeMove(Character c)
 	{
 		Set<State> possibleStatesByChar = transitionMap.get(c);
 		if(possibleStatesByChar == null)
 			return possibleStatesByChar;
 		
-		/* Should this new HashSet() part go in a private function? */
 		Set<State> possibleStates = new HashSet<State>(possibleStatesByChar);
 		
 		for(State possibleStateByChar : possibleStatesByChar)
@@ -62,5 +79,63 @@ public class State {
 		
 		return possibleStates;
 		
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		System.out.println("This is running");
+		State state1 = new State();
+		State state2 = new State(); 
+		State state3 = new State();
+		
+		System.out.println("State 1 : " + state1);
+		System.out.println("State 2 : " + state2);
+		System.out.println("State 3 : " + state3);
+		
+		Set<State> moves1 = new HashSet<State>();
+		moves1.add(state2);
+		
+		state1.addMove('b', moves1);
+		
+		Set<State> moves2 = new HashSet<State>();
+		moves2.add(state2);
+		moves2.add(state3);
+		
+		state2.addMove('b', moves2);
+		
+		Set<State> moves = state1.makeMove('b');
+		System.out.println(moves.size());
+		
+		for(State move : moves)
+			System.out.println(move);
+				
+		moves = state2.makeMove('b');
+		System.out.println(moves.size());
+		
+		for(State move : moves)
+			System.out.println(move);
+		
+		moves = state2.makeMove('c');
+		if(moves == null)
+			System.out.println("No move" );
+		else
+		{
+			System.out.println(moves.size());
+		
+			for(State move : moves)
+				System.out.println(move);
+		}
+		
+		Set<State> emptyMove = new HashSet<State>();
+		emptyMove.add(state1);
+		
+		state3.addMove('\0', emptyMove);
+				
+		moves = state2.makeMove('b');
+		System.out.println(moves.size());
+		
+		for(State move : moves)
+			System.out.println(move);
 	}
 }
